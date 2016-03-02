babf1794de2981efbbd17f069fca0d6c41e3f053
edu <eduardo.abizeid@gmail.com>
2016-03-02 15:39:35 -0300

    fix bug

--- src/Kells/Bundle/FrontBundle/Controller/DefaultController.php
+++ src/Kells/Bundle/FrontBundle/Controller/DefaultController.php
@@ -5,7 +5,7 @@ namespace Kells\Bundle\FrontBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Security\Core\SecurityContext;Model
+use Symfony\Component\Security\Core\SecurityContext;
 
 use Kells\Bundle\FrontBundle\Form\Type\SearchType;
 use Kells\Bundle\FrontBundle\Form\Model\Search;

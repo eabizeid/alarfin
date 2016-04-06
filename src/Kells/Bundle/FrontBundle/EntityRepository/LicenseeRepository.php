58b415b35d23bd1b0ee9d4c055831d68de04b3f0
edu <eduardo.abizeid@gmail.com>
2016-04-06 15:05:17 -0300

    putting title of city correctly

---
+++ src/Kells/Bundle/FrontBundle/EntityRepository/LicenseeRepository.php
@@ -0,0 +1,20 @@
+<?php
+namespace Kells\Bundle\FrontBundle\EntityRepository;
+
+use Doctrine\ORM\EntityRepository;
+
+class CityRepository extends EntityRepository {
+	
+ public function findAll()
+    {
+	
+    $em = $this->getEntityManager();
+
+    $query = $em->createQuery('
+        SELECT *
+        FROM KellsFrontBundle:Licensee l
+        ORDER BY l.fantasyName ASC, l.city.description  ASC
+    ');
+    	SC'));
+    }
+}

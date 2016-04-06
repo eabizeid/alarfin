c721f89084b005722b40c97b422970f7fc5da8e2
edu <eduardo.abizeid@gmail.com>
2016-04-06 15:24:28 -0300

    putting title of city correctly

--- src/Kells/Bundle/FrontBundle/EntityRepository/LicenseeRepository.php
+++ src/Kells/Bundle/FrontBundle/EntityRepository/LicenseeRepository.php
@@ -7,8 +7,10 @@ class LicenseeRepository extends EntityRepository {
 	
  public function findAll() {
 	
-      return $this->createQueryBuilder('l')
-        ->leftJoin('l.city','c')
+      return $this->createQueryBuilder()
+        ->select('l')
+        ->from('Licensee', 'l')
+        ->join('l.City', 'c')
         ->addOrderBy('c.description', 'ASC')
         ->addOrderBy('l.fantasyName', 'ASC')
         ->getQuery() 
